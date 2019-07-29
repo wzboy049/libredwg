ec1539a8b908caae31f2e211d069b882c05ab8d4
Reini Urban <rurban@cpan.org>
2019-07-29 15:44:04 +0200

    WIP more decode_object protections
    check against bitsize 0,
    better error on read_H overflow

--- src/decode.c
+++ src/decode.c
@@ -3570,6 +3570,11 @@ dwg_decode_entity (Bit_Chain *dat, Bit_Chain *hdl_dat, Bit_Chain *str_dat,
   LOG_TRACE ("handle: " FORMAT_H " [H 5]\n", ARGS_H (obj->handle))
 
   PRE (R_13) { return DWG_ERR_NOTYETSUPPORTED; }
+  if (!obj->bitsize)
+    {
+      LOG_ERROR ("Invalid entity bitsize 0");
+      return error | DWG_ERR_INTERNALERROR;
+    }
 
   error |= dwg_decode_eed (dat, (Dwg_Object_Object *)ent);
   if (error & (DWG_ERR_INVALIDEED | DWG_ERR_VALUEOUTOFBOUNDS))
@@ -3664,6 +3669,11 @@ dwg_decode_object (Bit_Chain *dat, Bit_Chain *hdl_dat, Bit_Chain *str_dat,
         error |= DWG_ERR_VALUEOUTOFBOUNDS;
       }
   }
+  if (!obj->bitsize)
+    {
+      LOG_ERROR ("Invalid object bitsize 0");
+      return error | DWG_ERR_INTERNALERROR;
+    }
   // documentation bug, same BL type as with entity
   FIELD_BL (num_reactors, 0);
   SINCE (R_2010)
